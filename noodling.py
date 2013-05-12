818938138bdffa04fbc22ca75f5fa4120b018d31
Ben Mordue <benmordue@gmail.com>
2013-05-12 19:43:49 +0100

    Noodling

--- noodling.py
+++ noodling.py
@@ -1,11 +1,16 @@
 import twitter
 import config
+import ttp
 
 request_token_URL = "https://api.twitter.com/oauth/request_token"
 authorize_URL = "https://api.twitter.com/oauth/authorize"
 access_token_URL = "https://api.twitter.com/oauth/access_token"
 
+PAGE_SIZE = 20
+
 api = twitter.Api()
+
+favs = []
 
 def twitter_login():
 	api = twitter.Api(consumer_key =      config.TWITTER_CONSUMER_KEY,
@@ -14,6 +19,20 @@ def twitter_login():
 					access_token_secret = config.TWITTER_CONSUMER_SECRET)
 	print 'Logged in'
 
+def get_all_favs(user):
+	
+	theUser = api.GetUser(user)
+	maxPage = theUser.favourites_count / PAGE_SIZE + 1
+	favs = []
+	for p in range(maxPage):		
+		favs.append( api.GetFavorites(user, page = p) )
+		
+	print len(favs)
+
+def get_urls(tweet):
+	p = ttp.Parser
+	result = p.parse(tweet.AsDict()['text'])
+	return result.urls
 
 def main():
 	twitter_login()
