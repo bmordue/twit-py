b7c545d62f4651553feeff8eb660f17e5307a695
Ben Mordue <benmordue@gmail.com>
2013-05-12 18:37:08 +0100

    Log in

--- noodling.py
+++ noodling.py
@@ -1,9 +1,24 @@
 import twitter
+import config
 
 request_token_URL = "https://api.twitter.com/oauth/request_token"
 authorize_URL = "https://api.twitter.com/oauth/authorize"
 access_token_URL = "https://api.twitter.com/oauth/access_token"
 
-def twitter_login:
-	api = twitter.Api()
+api = twitter.Api()
+
+def twitter_login():
+	api = twitter.Api(consumer_key =      config.TWITTER_CONSUMER_KEY,
+					consumer_secret =     config.TWITTER_CONSUMER_SECRET, 
+					access_token_key =	  config.TWITTER_CONSUMER_KEY, 
+					access_token_secret = config.TWITTER_CONSUMER_SECRET)
+	print 'Logged in'
+
+
+def main():
+	twitter_login()
 	
+if __name__ == "__main__":
+	main()
+
+
